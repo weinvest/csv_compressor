960bb2110105fb2c89760607b44f483fa2967151
li_shugan <li_shugan@126.com>
2016-03-10 22:06:33 +0800

    改进了ColumnCompressor

--- ColumnCompressor.cpp
+++ ColumnCompressor.cpp
@@ -1,9 +1,13 @@
+#include <boost/iostreams/filter/bzip2.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/mapped_region.hpp> 
 #include "ColumnCompressor.h"
-
+namespace bip = boost::interprocess;
 ColumnCompressor::ColumnCompressor(char delimiter, size_t nBlockRows)
     :mBlockRows(nBlockRows)
-    ,mTotalRows(0)
     ,mTotalColumns(0)
+    ,mTotalRows(0)
     ,mCurrentRowsInBlock(0)
     ,mDelimiter(delimiter)
 {
@@ -12,10 +16,49 @@ ColumnCompressor::ColumnCompressor(char delimiter, size_t nBlockRows)
 
 void ColumnCompressor::Compress(const std::string& inputFileName, const std::string& outputFileName)
 {
+    //将输入文件映射到内存
+    bip::file_mapping inputFile(inputFileName.c_str(), bip::read_only);  
+    bip::mapped_region inputFileRegion(inputFile, bip::read_write);  
+	  
+    const char* startAddress = static_cast<const char*>(inputFileRegion.get_address());  
+    auto size  = inputFileRegion.get_size(); 
+    const char* endAddress = startAddress + size;
+    auto currentAddress = startAddress;
+    
+    if(currentAddress == endAddress)
+    {
+	return; //空文件
+    }
+
+    //初始化输出流
     mOutputStream.reset(new bio::filtering_ostream());
+    mOutputStream->push(bio::bzip2_decompressor());
+    mOutputStream->push(bio::file_sink(outputFileName));
+
+    //读入header
+    Columns columns;
+    bool hasMoreData = Next(currentAddress, endAddress, columns, mTotalColumns);
+
+    //写入元信息
+    mOutputStream->write(reinterpret_cast<const char*>(&mTotalColumns), sizeof(mTotalColumns) + sizeof(mBlockRows));
+    mOutputStream->write(mNewLine, 1);
+
+    //写入文件头
+    mOutputStream->write(startAddress, currentAddress - startAddress + 1);
+
+    if(!hasMoreData)
+    {
+	return;
+    }
+
+    //读入第一行
+    size_t columnCount(0);
+    hasMoreData = Next(currentAddress, endAddress, columns, columnCount);
+
+    //开始每mBlockRows一压缩
 }
 
-bool ColumnCompressor::Next(const char* currentAddress, const char* endAddress, std::array<const char*, MAX_COLUMNS>& spliters, size_t& columns)
+bool ColumnCompressor::Next(const char*& currentAddress, const char* endAddress, Columns& spliters, size_t& columns)
 {
     columns = 0;
     spliters[0] = ++currentAddress;
@@ -27,6 +70,7 @@ bool ColumnCompressor::Next(const char* currentAddress, const char* endAddress, 
         }
         ++currentAddress;
     }
+    spliters[columns] = currentAddress;
 
     return currentAddress != endAddress;
 }
--- ColumnCompressor.h
+++ ColumnCompressor.h
@@ -1,5 +1,6 @@
 #ifndef COLUMNCOMPRESSOR_H
 #define COLUMNCOMPRESSOR_H
+#include <sstream>
 #include <memory>
 #include <array>
 #include <boost/iostreams/filtering_stream.hpp>
@@ -8,23 +9,29 @@ namespace bio = boost::iostreams;
 class ColumnCompressor
 {
 public:
-
+    typedef std::array<const char*, MAX_COLUMNS> Columns;
     ColumnCompressor(char delimiter, size_t nBlockRows);
 
     void Compress(const std::string& inputFileName, const std::string& outputFileName);
 private:
 
-    bool Next(const char* currentAddress,const char* endAddress, std::array<const char*, MAX_COLUMNS>& spliters, size_t& columns);
+    bool Next(const char*& currentAddress,const char* endAddress, Columns& spliters, size_t& columns);
 
 
+    //下面这两个变量的位置不能改变
     const size_t mBlockRows;
-    size_t mTotalRows;
     size_t mTotalColumns;
+
+    size_t mTotalRows;
     size_t mCurrentRowsInBlock;
 
-    enum type
+    struct ColumnInfo
     {
-        INT, DOUBLE, STRING
+	enum type
+	{
+	    INT, DOUBLE, STRING
+	}Type;
+	std::stringstream ColumnStream;
     };
 
     std::shared_ptr<bio::filtering_ostream> mOutputStream;
